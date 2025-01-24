86b74eece3fb03052e5663efd92a4307e6fbf3c4
Octavio Rafael Cibrián Osuna <octavio.cibrian@gmail.com>
2025-01-24 07:15:16 -0500

    Add Change the TimeOut for Interval

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,10 @@
 import { useImperativeHandle, useRef } from "react"
 
-export default function ResultModal({ref, result, targetTime}) {
+export default function ResultModal({ref, targetTime, remainingTime, onReset}) {
     const dialog = useRef();
-
+    const userLost = remainingTime <= 0;
+    const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1- remainingTime / (targetTime*1000)) *100);
     useImperativeHandle(ref, ()=> {
         return {
             open() {
@@ -11,12 +13,13 @@ export default function ResultModal({ref, result, targetTime}) {
         }
     });
 
-    return <dialog ref={dialog} className="result-modal">
-        <h2>Your {result}:</h2>
+    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
+        {userLost && <h2>Your Lost</h2>}
+        {!userLost && <h2>Your score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
-        <p>You stopped the timer with <strong>X seconds left.</strong></p>
-        <form method="dialog">
-            <button>Close</button>
+        <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+        <form method="dialog" onSubmit={onReset}>
+            <button >Close</button>
         </form>
     </dialog>
 }
